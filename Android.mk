11c2f80f2547daf3cc626f8d4e40b8f9e21101aa
Tec4Sho <tec4sho@gmail.com>
2025-08-14 10:44:39 -0400

    updated TeamWin-TWRP.yml Recovery Build (Legacy).yml

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@
 ifneq ($(filter tb8163p3_bsp,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := device/alps/tb8163p3_bsp
+COMMON_LUNCH_CHOICES := omni_tb8163p3_bsp-eng
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
