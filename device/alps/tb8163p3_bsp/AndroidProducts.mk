3ac190417e7f0a4294d415d8314e1fd1c7f39369
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-08-15 15:05:33 -0400

    Update AndroidProducts.mk

--- device/alps/tb8163p3_bsp/AndroidProducts.mk
+++ device/alps/tb8163p3_bsp/AndroidProducts.mk
@@ -2,6 +2,6 @@
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
 # The part of last line in mine looks like "omni_Primo_RX5.mk"
 
-LOCAL_PATH := tree/android-9.0/device/alps/tb8163p3_bsp
+LOCAL_PATH := device/alps/tb8163p3_bsp
 
 PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_tb8163p3_bsp.mk
