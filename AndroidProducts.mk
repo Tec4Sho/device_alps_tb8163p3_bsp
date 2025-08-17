64489d72add7af47d3754225d3aaf0965cddf6aa
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-08-17 12:15:04 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 # Replace $$DEVICE$$ with your Device Name's Value.
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
 # The part of last line in mine looks like "omni_Primo_RX5.mk"
-LOCAL_PATH := $(call my-dir)
-
-PRODUCT_MAKEFILES := ${{ LOCAL_PATH }}omni_tb8163p3.mk
+LOCAL_PATH := android_device_alps_tb8163p3
+PRODUCT_MAKEFILES := ${{ LOCAL_PATH }}/omni_tb8163p3.mk
