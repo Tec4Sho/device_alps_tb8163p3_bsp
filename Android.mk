2670f626069a8379a09014b2e3e9756f39313aeb
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-08-17 04:14:18 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,13 @@
 # Replace $$DEVICE$$ with your Device Name's Value. Mine is Primo_RX5.
 # Replace $$BRAND$$ with your Brand's / Manufacturer's Value, Mine is WALTON 
 
-ifneq ($(filter tb8163p3,$(TARGET_DEVICE)),)
+ifneq ($(filter android_device_alps_tb8163p3,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
-COMMON_LUNCH_CHOICES := omni_tb8163p3-eng
+COMMON_LUNCH_CHOICES := \
+omni_tb8163p3-eng \
+omni_tb8163p3.mk
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
