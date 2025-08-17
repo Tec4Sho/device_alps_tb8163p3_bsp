a7dbf52c2a024db6e47837dcefcbae1d1b1ff595
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-08-17 04:16:01 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,4 +3,4 @@
 # The part of last line in mine looks like "omni_Primo_RX5.mk"
 LOCAL_PATH := $(call my-dir)
 
-PRODUCT_MAKEFILES := ${{ LOCAL_PATH }}/omni_tb8163p3.mk
+PRODUCT_MAKEFILES := ${{ LOCAL_PATH }}omni_tb8163p3.mk
